fdb4d41131e5c7b422520c981c4176fcf5e2dc32
vallops99 <valerio.farrotti@gmail.com>
2025-03-20 22:01:09 +0100

    Adding airgapping

--- flake.nix
+++ flake.nix
@@ -1,17 +1,36 @@
 {
-  description = "Minimal NixOS installation media";
+  description = "Airgapped NixOS";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   outputs = { self, nixpkgs }: {
-    nixosConfigurations = {
-      exampleIso = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ({ pkgs, modulesPath, ... }: {
-            imports = [ (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix") ];
-            environment.systemPackages = [ pkgs.sparrow ];
-          })
-        ];
-      };
+    nixosConfigurations.airgapped-iso = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ({ pkgs, modulesPath, ... }: {
+          imports = [ (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix") ];
+          environment.systemPackages = [ pkgs.sparrow ];
+
+          boot.blacklistedKernelModules =
+            let
+              modulesFile = builtins.readFile ./blacklist.txt;
+              # Filter out empty lines and whitespace
+              modulesList = lib.filter
+                (x: x != "" && builtins.match "^[ \t]*$" x == null)
+                (lib.splitString "\n" modulesFile);
+            in modulesList;
+
+          networking.networkmanager.enable = false;
+
+          hardware.bluetooth.enable = false;
+
+          networking.interfaces = {};
+          networking.firewall = {
+            enable = true;
+            allowedTCPPorts = [];
+            allowedUDPPorts = [];
+            rejectPackets = true;
+          };
+        })
+      ];
     };
   };
 }
